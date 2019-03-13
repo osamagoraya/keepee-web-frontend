5062bad2e73ccaf2e05393ac8a613d13e6de9233
Ali Maqsood <ali.maqsood@conradlabs.com>
2019-03-13 21:41:50 +0500

    Added instructions for the table

--- src/Components/Common/Table.js
+++ src/Components/Common/Table.js
@@ -2,5 +2,16 @@ import OBootstrapTable from 'react-bootstrap-table-next';
 import OcellEditFactory from 'react-bootstrap-table2-editor';
 import './Common.css';
 
+/**
+ * -- Bootstrap Table classess --
+ * headerClasses="k-header-row"
+ * wrapperClasses="k-table-container"
+ * 
+ * -- Column classes --
+ * headerClasses: 'k-header-cell'
+ * classes: 'k-body-cell'
+ * formatter: (cell, row, index) => <div className='k-force'>{cell}</div>
+ */
+
 export const BootstrapTable = OBootstrapTable;
 export const cellEditFactory = OcellEditFactory;
