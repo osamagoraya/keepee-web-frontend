6704c18505bf74cbe0b0306d57da32fb5cfaf426
Ali Maqsood <ali.maqsood@conradlabs.com>
2019-04-03 18:27:56 +0500

    Added stubs to add and update category. Need to integrate with api.

--- src/Components/Categories/Categories.js
+++ src/Components/Categories/Categories.js
@@ -42,91 +42,159 @@ class Categories extends Component {
     );
   }
 
-  onCategoryUpdate = (oldValue, newValue, row, column) => {
-    console.log("Category data updated",oldValue, newValue, row, column);
+  updateCategory(category) {
+    console.log("Updating category:", category);
+    this.setState({apiCallInProgress: true, apiCallType: 'update'});
+    sendAuthenticatedAsyncRequest(  
+      "/updateCategory",
+      "POST", 
+      category,
+      (r) => {
+        console.log("Category updated", r);
+        //TODO: update categories in state, or do we need to?
+        this.setState({apiCallInProgress: false, apiCallType: 'none'})
+      },
+      (r) => {
+        console.log("Category update failed", r);
+        alert("Category update failed");
+        //TODO: remove category data from state
+        this.setState({apiCallInProgress: false, apiCallType: 'none'})
+      },
+    );
   }
 
+  addCategory(category) {
+    console.log("Adding category:", category);
+    this.setState({apiCallInProgress: true, apiCallType: 'add'});
+    sendAuthenticatedAsyncRequest(  
+      "/addCategory",
+      "POST", 
+      category,
+      (r) => {
+        console.log("Category added", r);
+        //TODO: must have received category id 
+        this.setState({apiCallInProgress: false, apiCallType: 'none'})
+      },
+      (r) => {
+        console.log("Category addition failed", r);
+        alert("Category addition failed");
+        //TODO: remove category data from state
+        this.setState({apiCallInProgress: false, apiCallType: 'none'})
+      },
+    );
+  }
+
+  onCategoryUpdate = (oldValue, newValue, row, column) => {
+    console.log("Category data updated",oldValue, newValue, row, column);
+    if (row.id === -1) {
+      // adding new category
+      if (!row.name || !row.vat || !row.categoryNo){
+        console.log("incomplete data, not adding category")
+      } else {
+        row.id = undefined;
+        // this.addCategory(row);
+      }
+    } else {
+      // updating existing category
+      // this.updateCategory(row);
+    }
+  }
+
+  columns = [
+    {
+      dataField: 'id',
+      text: 'Id',
+      headerClasses: 'k-header-cell',
+      classes: 'k-body-cell',
+      formatter: (cell, row, index) => <div className='k-force'>{cell}</div>,
+      editCellClasses: 'k-edit-cell',
+      hidden: true,
+    }, {
+      dataField: 'name',
+      text: 'Category',
+      headerClasses: 'k-header-cell',
+      classes: 'k-body-cell',
+      formatter: (cell, row, index) => <div className='k-force'>{cell}</div>,
+      editCellClasses: 'k-edit-cell'
+    }, {
+      dataField: 'group',
+      text: 'Group',
+      headerClasses: 'k-header-cell',
+      classes: 'k-body-cell',
+      headerStyle: { width: '10%' },
+      formatter: (cell, row, index) => <div className='k-force'>{cell}</div>,
+      editCellClasses: 'k-edit-cell'
+    }, {
+      dataField: 'subGroup',
+      text: 'sub Group',
+      headerClasses: 'k-header-cell',
+      classes: 'k-body-cell',
+      formatter: (cell, row, index) => <div className='k-force'>{cell}</div>,
+      editCellClasses: 'k-edit-cell'
+    },{
+      dataField: 'vat',
+      text: 'Vat %',
+      headerClasses: 'k-header-cell',
+      classes: 'k-body-cell',
+      formatter: (cell, row, index) => <div className='k-force'>{cell}</div>,
+      editCellClasses: 'k-edit-cell'
+    },{
+      dataField: 'taxAdvances',
+      text: 'Tax Advances',
+      headerClasses: 'k-header-cell',
+      classes: 'k-body-cell',
+      formatter: (cell, row, index) => <div className='k-force'>{cell}</div>,
+      editCellClasses: 'k-edit-cell'
+    },{
+      dataField: 'withHolding',
+      text: 'With holding',
+      headerClasses: 'k-header-cell',
+      classes: 'k-body-cell',
+      formatter: (cell, row, index) => <div className='k-force'>{cell}</div>,
+      editCellClasses: 'k-edit-cell'
+    },{
+      dataField: 'categoryNo',
+      text: 'Category No',
+      headerClasses: 'k-header-cell',
+      classes: 'k-body-cell',
+      formatter: (cell, row, index) => <div className='k-force'>{cell}</div>,
+      editCellClasses: 'k-edit-cell'
+    },{
+      dataField: 'type',
+      text: 'Type',
+      headerClasses: 'k-header-cell',
+      classes: 'k-body-cell',
+      formatter: (cell, row, index) => <div className='k-force'>{cell}</div>,
+      editCellClasses: 'k-edit-cell',
+      hidden: true
+    },{
+      dataField: 'vatCategoryNo',
+      text: 'Vat Category No',
+      headerClasses: 'k-header-cell',
+      classes: 'k-body-cell',
+      formatter: (cell, row, index) => <div className='k-force'>{cell}</div>,
+      editCellClasses: 'k-edit-cell'
+    },
+  ];
+
+  addRow = () => {
+    const {categories} = this.state;
+    if (categories.filter(c => c.id === -1).length > 0)
+      alert ("Please complete data for previously added category");
+    else {
+      const emptyCat = {
+        id: -1, name: "", group: "", subGroup: "", vat: "", taxAdvances: "",withHolding: "", categoryNo: "",type: "", vatCategoryNo: ""
+      }
+      categories.splice(0, 0, emptyCat);
+      this.setState({
+        categories: categories
+      });
+    }
+  }
   render() {
     const {categories, apiCallInProgress, apiCallType} = this.state;
 
-    const columns = [
-      {
-        dataField: 'id',
-        text: 'Id',
-        headerClasses: 'k-header-cell',
-        headerAlign: 'center',
-        classes: 'k-body-cell',
-        style: {textAlign: 'center'},
-        formatter: (cell, row, index) => <div className='k-force'>{cell}</div>,
-        editCellClasses: 'k-edit-cell',
-        hidden: true,
-      }, {
-        dataField: 'name',
-        text: 'Category',
-        headerClasses: 'k-header-cell',
-        classes: 'k-body-cell',
-        formatter: (cell, row, index) => <div className='k-force'>{cell}</div>,
-        editCellClasses: 'k-edit-cell'
-      }, {
-        dataField: 'group',
-        text: 'Group',
-        headerClasses: 'k-header-cell',
-        classes: 'k-body-cell',
-        headerStyle: { width: '10%' },
-        formatter: (cell, row, index) => <div className='k-force'>{cell}</div>,
-        editCellClasses: 'k-edit-cell'
-      }, {
-        dataField: 'subGroup',
-        text: 'sub Group',
-        headerClasses: 'k-header-cell',
-        classes: 'k-body-cell',
-        formatter: (cell, row, index) => <div className='k-force'>{cell}</div>,
-        editCellClasses: 'k-edit-cell'
-      },{
-        dataField: 'vat',
-        text: 'Vat %',
-        headerClasses: 'k-header-cell',
-        classes: 'k-body-cell',
-        formatter: (cell, row, index) => <div className='k-force'>{cell}</div>,
-        editCellClasses: 'k-edit-cell'
-      },{
-        dataField: 'taxAdvances',
-        text: 'Tax Advances',
-        headerClasses: 'k-header-cell',
-        classes: 'k-body-cell',
-        formatter: (cell, row, index) => <div className='k-force'>{cell}</div>,
-        editCellClasses: 'k-edit-cell'
-      },{
-        dataField: 'withHolding',
-        text: 'With holding',
-        headerClasses: 'k-header-cell',
-        classes: 'k-body-cell',
-        formatter: (cell, row, index) => <div className='k-force'>{cell}</div>,
-        editCellClasses: 'k-edit-cell'
-      },{
-        dataField: 'categoryNo',
-        text: 'Category No',
-        headerClasses: 'k-header-cell',
-        classes: 'k-body-cell',
-        formatter: (cell, row, index) => <div className='k-force'>{cell}</div>,
-        editCellClasses: 'k-edit-cell'
-      },{
-        dataField: 'type',
-        text: 'Type',
-        headerClasses: 'k-header-cell',
-        classes: 'k-body-cell',
-        formatter: (cell, row, index) => <div className='k-force'>{cell}</div>,
-        editCellClasses: 'k-edit-cell',
-        hidden: true
-      },{
-        dataField: 'vatCategoryNo',
-        text: 'Vat Category No',
-        headerClasses: 'k-header-cell',
-        classes: 'k-body-cell',
-        formatter: (cell, row, index) => <div className='k-force'>{cell}</div>,
-        editCellClasses: 'k-edit-cell'
-      },
-    ];
+    
     const cellEdit = cellEditFactory({
       mode: 'click',
       afterSaveCell: this.onCategoryUpdate,
@@ -134,32 +202,27 @@ class Categories extends Component {
     });
     return (
       <div className="canvas-container categories-container">
-      <Caption style={{
-          marginLeft: '60px',
-          marginBottom: '10px',
-        }}>
-          {apiCallInProgress && apiCallType === 'fetch' 
-            ? "Fetching Categories ..."
-            : null
-          }
-        </Caption>
+      {apiCallInProgress && apiCallType === 'fetch' 
+        ? <Caption style={{ marginLeft: '60px', marginBottom: '10px', }}> Fetching Categories ...</Caption>
+        : null
+      }
       {
         (apiCallInProgress && apiCallType === 'fetch') || !categories
         ? null 
         : <span>
+          <Button className="top-right-fab-btn" fab disabled={apiCallInProgress} onClick={this.addRow}>
+            <AddIcon />
+          </Button>
           <BootstrapTable 
             // caption={<CaptionElement className="a"/>} 
             keyField='id' 
             data={categories} 
-            columns={columns} 
+            columns={this.columns} 
             bordered={false}
             headerClasses="k-header-row"
             wrapperClasses="k-table-container"
             cellEdit={cellEdit}
             /> 
-            <Button className="left-bottom-fab-btn" fab>
-              <AddIcon />
-            </Button>
           </span>
       }
       </div>
